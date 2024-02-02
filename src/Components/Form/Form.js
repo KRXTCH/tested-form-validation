dd850ebd888b387bc2b80783f78dd481c28b68d0
Lorys Jelezian <lorysjele@gmail.com>
2024-02-02 17:34:11 +0100

    Finished version

---
+++ src/Components/Form/Form.js
@@ -0,0 +1,188 @@
+import React from "react";
+import { birthDateValidator } from "../../Validators/BirthDateValidator/birthDateValidator";
+import { mailValidator } from "../../Validators/MailValidator/mailValidator";
+import { nameValidator } from "../../Validators/NameValidator/nameValidator";
+import { postalCodeValidator } from "../../Validators/PostalCodeValidator/postalCodeValidator";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+function Form() {
+    const [formData, setFormData] = React.useState({
+      birthDate: '',
+      email: '',
+      name: '',
+      surname: '',
+      city: '',
+      postalCode: ''
+    });
+  
+    const [validationResults, setValidationResults] = React.useState({
+      isBirthDateValid: true,
+      isEmailValid: true,
+      isNameValid: true,
+      isSurnameValid: true,
+      isCityValid: true,
+      isPostalCodeValid: true
+    });
+
+    const [isFormValid, setIsFormValid] = React.useState(false);
+
+    React.useEffect(() => {
+      const allFieldsFilled = Object.values(formData).every((value) => value.trim() !== '');
+      setIsFormValid(allFieldsFilled);
+    }, [formData]);
+
+    const saveFormData = () => {
+      localStorage.setItem('formData', JSON.stringify(formData));
+
+      setFormData({
+        birthDate: '',
+        email: '',
+        name: '',
+        surname: '',
+        city: '',
+        postalCode: ''
+      });  
+  
+      toast.success('Formulaire soumis avec succès !');
+    };
+  
+    const handleInputChange = (e) => {
+      const { name, value } = e.target;
+      
+      if (value == '') {
+        setIsFormValid(false);
+      } else {
+        setIsFormValid(true);
+      }
+
+      setFormData((prevData) => ({
+        ...prevData,
+        [name]: value,
+      }));
+    };
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+  
+      const isBirthDateValid = birthDateValidator(formData.birthDate);
+      const isEmailValid = mailValidator(formData.email);
+      const isNameValid = nameValidator(formData.name);
+      const isSurnameValid = nameValidator(formData.surname);
+      const isCityValid = nameValidator(formData.city);
+      const isPostalCodeValid = postalCodeValidator(formData.postalCode);
+  
+      setValidationResults({
+        isBirthDateValid,
+        isEmailValid,
+        isNameValid,
+        isSurnameValid,
+        isCityValid,
+        isPostalCodeValid
+      });
+
+      const formIsValid = isBirthDateValid && isEmailValid && isNameValid && isSurnameValid && isCityValid && isPostalCodeValid;
+  
+      if (formIsValid) {
+        saveFormData();
+      } else {
+        toast.error('Veuillez remplir correctement tous les champs du formulaire.');
+        localStorage.removeItem('formData');
+      }
+    };
+  
+    return (
+      <div>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="birthDate">Date de naissance:</label>
+            <input
+              id="birthDate"
+              type="text"
+              name="birthDate"
+              value={formData.birthDate}
+              onChange={handleInputChange}
+            />
+            {!validationResults.isBirthDateValid && (
+              <span style={{ color: 'red' }}>Date de naissance invalide</span>
+            )}
+          </div>
+    
+          <div>
+            <label htmlFor="email">Adresse e-mail:</label>
+            <input
+              id="email"
+              type="text"
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
+            />
+            {!validationResults.isEmailValid && (
+              <span style={{ color: 'red' }}>Adresse e-mail invalide</span>
+            )}
+          </div>
+    
+          <div>
+            <label htmlFor="name">Nom:</label>
+            <input
+              id="name"
+              type="text"
+              name="name"
+              value={formData.name}
+              onChange={handleInputChange}
+            />
+            {!validationResults.isNameValid && (
+              <span style={{ color: 'red' }}>Nom invalide</span>
+            )}
+          </div>
+
+          <div>
+            <label htmlFor="surname">Prenom:</label>
+            <input
+              id="surname"
+              type="text"
+              name="surname"
+              value={formData.surname}
+              onChange={handleInputChange}
+            />
+            {!validationResults.isSurnameValid && (
+              <span style={{ color: 'red' }}>Prenom invalide</span>
+            )}
+          </div>
+
+          <div>
+            <label htmlFor="city">Ville:</label>
+            <input
+              id="city"
+              type="text"
+              name="city"
+              value={formData.city}
+              onChange={handleInputChange}
+            />
+            {!validationResults.isCityValid && (
+              <span style={{ color: 'red' }}>Ville invalide</span>
+            )}
+          </div>
+    
+          <div>
+            <label htmlFor="postalCode">Code postal:</label>
+            <input
+              id="postalCode"
+              type="text"
+              name="postalCode"
+              value={formData.postalCode}
+              onChange={handleInputChange}
+            />
+            {!validationResults.isPostalCodeValid && (
+              <span style={{ color: 'red' }}>Code postal invalide</span>
+            )}
+          </div>
+    
+          <button type="submit" disabled={!isFormValid}>Soumettre</button>
+        </form>
+        <ToastContainer />
+      </div>
+    );
+  }
+  
+  export default Form;
---
+++ src/Validators/BirthDateValidator/birthDateValidator.test.js
@@ -0,0 +1,52 @@
+import { birthDateValidator } from "./birthDateValidator";
+import { calculateAge } from "./birthDateValidator";
+
+/**
+* @function birthDateValidator
+*/
+describe('birthDateValidator function', () => {
+    test('Valid birth date format should return true', () => {
+        const validBirthDate = '1990-01-01';
+        expect(birthDateValidator(validBirthDate)).toBe(true);
+    });
+
+    test('Invalid birth date with incorrect format should return false', () => {
+        const invalidBirthDate = '01/01/1990';
+        expect(birthDateValidator(invalidBirthDate)).toBe(false);
+    });
+
+    test('Invalid birth date with non-existent date should return false', () => {
+        const invalidBirthDate = '2022-02-48';
+        expect(birthDateValidator(invalidBirthDate)).toBe(false);
+    });
+
+    test('Invalid birth date with non-string input should return false', () => {
+        const nonStringInput = 12345;
+        expect(birthDateValidator(nonStringInput)).toBe(false);
+    });
+
+    test('Empty string should return false', () => {
+        const emptyBirthDate = '';
+        expect(birthDateValidator(emptyBirthDate)).toBe(false);
+    });
+});
+
+/**
+* @function calculateAge
+*/
+describe('calculateAge', () => {
+    it('devrait retourner l\'âge correct pour une date valide', () => {
+        const age = calculateAge('1990-01-01');
+        expect(age).toEqual(expect.any(Number));
+    });
+
+    it('devrait retourner null pour une date non valide', () => {
+        const age = calculateAge('date_non_valide');
+        expect(age).toBeNull();
+    });
+
+    it('devrait retourner null pour une date non fournie', () => {
+        const age = calculateAge();
+        expect(age).toBeNull();
+    });
+});
